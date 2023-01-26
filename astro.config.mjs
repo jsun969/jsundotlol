662c419c9f002503e75d52945097d02325d91730
Justin Sun <i@jsun969.cn>
2023-01-26 20:50:41 +0800

    chore: add tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
+// https://astro.build/config
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+});
