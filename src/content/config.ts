5c27a378b17117f67b09a9a6c099e359292818e7
Justin Sun <i@jsun969.cn>
2023-01-27 21:59:32 +0800

    feat: add blog list page

---
+++ src/content/config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.string().transform((str) => new Date(str)),
+    tags: z.array(z.string()),
+    language: z.enum(['en', 'zh-cn']),
+  }),
+});
+
+export const collections = { blog: blogCollection };
