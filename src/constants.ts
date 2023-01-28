b7d2084d73e582ad58db1c36ee8bd29c169feb28
Justin Sun <i@jsun969.cn>
2023-01-29 00:55:25 +0800

    feat: update default description

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const AVATAR_IMAGE = 'https://github.com/jsun969.png';
 export const SITE_NAME = 'Jsun Dot LOL';
 export const TWITTER_ACCOUNT = '@jsun969';
-export const DESCRIPTION = "Justin Sun's personal website";
+export const DESCRIPTION = "LOL! Welcome to Justin Sun's personal website!";
