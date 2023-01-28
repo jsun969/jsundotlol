25661d9749e36a183ee3c5a693373f7a82c5f655
Justin Sun <i@jsun969.cn>
2023-01-28 20:02:20 +0800

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-// https://astro.build/config
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://jsun.lol',
+  integrations: [tailwind(), sitemap()],
 });
