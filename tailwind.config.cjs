55041a01c216127a0dee21b3830c52e1119f42d6
Justin Sun <i@jsun969.cn>
2023-01-28 16:57:50 +0800

    feat: add blog post page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      sans: ['Poppins'],
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
