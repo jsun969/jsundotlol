a4b6c894bca45bcc62836d1200d7a32e017c3ac9
Justin Sun <i@jsun969.cn>
2023-01-29 19:37:14 +0800

    feat: add header image for seo

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const AVATAR_IMAGE = 'https://github.com/jsun969.png';
+export const HEADER_IMAGE = 'https://jsun.lol/header.png';
 export const SITE_NAME = 'Jsun Dot LOL';
 export const TWITTER_ACCOUNT = '@jsun969';
 export const DESCRIPTION = "LOL! Welcome to Justin Sun's personal website!";
