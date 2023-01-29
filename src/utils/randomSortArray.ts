ecfb3127c20cc5a99d71223227aff0cd54dcd9b5
Justin Sun <i@jsun969.cn>
2023-01-29 19:39:15 +0800

    feat: sort friends array randomly

---
+++ src/utils/randomSortArray.ts
@@ -0,0 +1,8 @@
+export const randomSortArray = <T>(array: T[]) => {
+  const shuffledArray = structuredClone(array);
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+};
