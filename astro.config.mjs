5de7a61c651610df47c41207018d376e9a98ca90
Justin Sun <i@jsun969.cn>
2023-01-31 16:59:57 +0800

    chore: use vercel instead of cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
@@ -8,5 +8,5 @@ export default defineConfig({
   site: 'https://jsun.lol',
   integrations: [tailwind(), sitemap()],
   output: 'server',
-  adapter: cloudflare(),
+  adapter: vercel(),
 });
