c336e24390f9fe9aa8c8c4176d93923ccbfe17ab
Justin Sun <i@jsun969.cn>
2023-01-31 20:43:16 +0800

    feat(blog): add links to headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +10,16 @@ export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
   adapter: vercel(),
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'wrap',
+          test: ['h2', 'h3', 'h4', 'h5', 'h6'],
+        },
+      ],
+    ],
+  },
 });
