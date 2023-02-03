912f6c0c016221475b8d1efce22d560524bb19db
Justin Sun <i@jsun969.cn>
2023-02-03 19:54:33 +0800

    fix: add random sort return type

--- src/utils/randomSortArray.ts
+++ src/utils/randomSortArray.ts
@@ -1,5 +1,5 @@
 export const randomSortArray = <T>(array: T[]) => {
-  const shuffledArray = structuredClone(array);
+  const shuffledArray: T[] = structuredClone(array);
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
