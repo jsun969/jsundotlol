a22655cf70eb01767019065171a0ee0d8c7058c0
Justin Sun <i@jsun969.cn>
2023-02-04 12:44:47 +0800

    feat(seo): add blog posts page to sitemap

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -1,10 +1,12 @@
+import glob from 'fast-glob';
+import path from 'path';
+
 import { SITE } from './constants';
 
 export const basePages = ['/', '/blog', '/tags', '/about', '/friends'];
 
-// TODO: Add blog posts pages to sitemap
-// const blogPages = (await getCollection('blog')).map(
-//   ({ slug }) => '/blog/' + slug,
-// );
+const blogPages = (await glob('./content/blog/*.md')).map(
+  (filePath) => '/blog/' + path.parse(filePath).name,
+);
 
-export const pages = basePages.map((url) => SITE + url);
+export const pages = [...basePages, ...blogPages].map((url) => SITE + url);
