756c5d6e7b2fc5d2bb2239cc7cc73bc8a55a4091
jsun969 <i@jsun.lol>
2026-01-18 15:09:18 +0800

    feat: remove astro things

--- src/utils/randomSortArray.ts
+++
@@ -1,8 +0,0 @@
-export const randomSortArray = <T>(array: T[]) => {
-  const shuffledArray: T[] = structuredClone(array);
-  for (let i = shuffledArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-  }
-  return shuffledArray;
-};
